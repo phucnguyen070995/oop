c40eabcab9d0eb88ec3244ccf13b486430b52616
phucnguyen <mac@Apples-MacBook-Pro.local>
2020-08-16 00:05:27 +0700

    add lab3

---
+++ lab_macos/lab3/lab3.2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Sap8Conhau
+{
+    class Program
+    {
+        //₫ịnh nghĩa các thuộc tính dữ liệu
+        const int MAX = 7; //chỉ số max của hàng và cột 
+        static int cachso = 0; //số cách sắp ₫ã tìm ₫ược
+        static int[] hang = new int[MAX + 1]; //danh sách chỉ số hàng ₫ã thử trong từng cột 
+        static int c; //vị trị cột ₫ang sắp
+        //Điểm nhập của chương trình
+        static void Main(string[] args)
+        {
+            // khởi ₫ộng trạng thái xuất phát
+            for (c = 0; c <= MAX; c++) hang[c] = -1;
+            cachso = 0;
+            //bắt ₫ầu ₫ặt con hậu ở cột 0 vào hàng 0 
+            hang[0] = 0;
+            //bắt ₫ầu tìm hàng cho con hậu ở cột thứ 2 (chỉ số là 1)
+            c = 1;
+            while (Tim1cach())
+            {
+                // tìm ₫ược cách sắp 8 con hậu
+                InKetqua();
+                //lùi lại cột trước ₫ể tiếp tục tìm cách sắp khác
+                c--;
+            }
+            // hết cách --> dừng chương trình
+            Console.WriteLine("Số nghiệm là " + cachso);
+            //chờ người dùng ấn Enter ₫ể ₫óng cửa sổ Console lại. 
+            Console.Write("Ấn Enter ₫ể dừng chương trình : ");
+            Console.Read();
+            return;
+        }
+        //cố gắng tìm 1 phương án sắp 8 con hậu
+        static bool Tim1cach()
+        {
+            int h;
+            while (c <= MAX)
+            {
+                // tìm chỉ số hàng cho con hậu ở cột c
+                h = timhang(c); if (h >= 0)
+                { //nếu tìm ₫ược
+                  //lưu hàng vào danh sách kết quả rồi tăng chỉ số cột ₫ể tiếp tục
+                    hang[c++] = h;
+                    continue;
+                }
+                //trường hợp không tìm ₫ược hàng cho con hậu ở cột c
+                if (c > 0)
+                {
+                    //nếu cột c không phải là cột ₫ầu tiên thì : 
+                    //xét lại cột c từ hàng ₫ầu rồi lùi cột c 
+                    hang[c--] = -1;
+                    continue;
+                }
+                //hết cách
+                return false;
+            }
+            return true;
+        }//hết hàm Tim1cach
+         //hàm cố gắng tìm chỉ số hàng cho con hậu ở cột c 
+         //trả về chỉ số hàng tìm ₫ược hay -1 ₫ể báo sai 
+        static int timhang(int c)
+        {
+            int h, hmin = hang[c] + 1;
+            for (h = hmin; h <= MAX; h++) //thửhàngh
+                if (testvitri(h, c))
+                    return h;
+            return -1;
+        }
+        //hàm kiểm tra xem có thể ₫ặt con hậu ở vị trí h,c ? //trả về TRUE nếu ₫ược, FALSE nếu không
+        static bool testvitri(int h, int c)
+        {
+            int c1, h1;
+            //cột c ₫ang kiểm tra nên chưa có con hậu nào 
+            // xem có con hậu nào nằm ở hàng h ?
+            for (c1 = 0; c1 < c; c1++)
+                if (hang[c1] == h) return false;
+            // xem có con hậu nào nằm trên ₫ường chéo trên trái - dưới phải 
+            c1 = c-1; h1 = h-1;
+            while (c1 >= 0 && h1 >= 0)
+            {
+                if (hang[c1] == h1) return false;
+                h1--; c1--;
+            }
+            // xem có con hậu nào nằm trên ₫ường chéo trên phải - dưới trái
+            c1 = c - 1; h1 = h + 1;
+            while (c1 >= 0 && h1 <= MAX)
+            {
+                if (hang[c1] == h1) return false;
+                h1++; c1--;
+            }
+            return true;
+        }
+        //hàm in kết quả 1 cách sắp 8 con hậu theo yêu cầu
+        static void InKetqua()
+        {
+            int h, c;
+            Console.WriteLine("Cách sắp thứ : " + (++cachso));
+            for (h = 0; h <= MAX; h++)
+            {
+                // Hiển thị hàng lưới ngang của bàn cờ
+                for (c = 0; c <= MAX; c++) Console.Write("+---");
+                Console.WriteLine("+");
+                // Hiển thị nội dung hàng thứ h của bàn cờ
+                for (c = 0; c <= MAX; c++)
+                    if (hang[c] == h) Console.Write("| x ");
+                    else Console.Write("| ");
+                Console.WriteLine("|");
+            }
+            // Hiển thị hàng lưới ngang cuối cùng của bàn cờ
+            for (c = 0; c <= MAX; c++) Console.Write("+---");
+            Console.WriteLine("+");
+        }
+    }//hết class Program 
+}//hết namespace
---
+++ lab_macos/lab3/lab3.3/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Madituan
+{
+    class Program
+    {
+        // Kiểu chứa thông tin về 1 bước ₫i
+        private struct ItemRec
+        {
+            public int x, y;
+            public int huong;
+        };
+        // Các biến dữ liệu chính
+        static int Size;
+        static int[,] Banco;
+        static ItemRec[] Nuocdi;
+        static int SoNuocdi;
+        static int SoNghiem;
+        // Chương trình chính
+        static void Main(string[] args)
+        {
+            Khoidong();
+            while (DiBuocNua())
+                if (SoNuocdi == Size * Size - 1)
+                {
+                    //tìm ₫ược 1 cách mới, in cách ₫i mới tìm ₫ược
+                    InKetqua();
+                    Banco[Nuocdi[SoNuocdi].x, Nuocdi[SoNuocdi].y] = -1; //lùi con mã 1 bước
+                    SoNuocdi = SoNuocdi - 1;
+                    //tiếp tục tìm cách ₫i khác
+                    Nuocdi[SoNuocdi].huong = Nuocdi[SoNuocdi].huong + 1;
+                }
+            //chờ người dùng ấn Enter ₫ể ₫óng cửa sổ Console lại.
+            Console.Write("Ấn Enter ₫ể dừng chương trình : ");
+            Console.Read();
+        }
+        //hàm khởi ₫ộng các giá tr5i ₫ầu của chương trình
+        static void Khoidong()
+        {
+            int i, j;
+            int row, col;
+            String buf;
+            do //chờ nhập kích thước bàn cờ 
+            {
+                Console.Write("Nhập số hàng (cột) của bàn cờ: ");
+                buf = Console.ReadLine(); Size = Int32.Parse(buf);
+            } while (Size <= 3 || Size > 9);
+            do //chờ nhập chỉ số hàng xuất phát
+            {
+                Console.Write("Nhập chỉ số hàng xuất phát của con mã : ");
+                buf = Console.ReadLine(); row = Int32.Parse(buf);
+            } while (row < 0 || Size <= row);
+            do //chờ nhập chỉ số cột xuất phát
+            {
+                Console.Write("Nhập chỉ số cột xuất phát của con mã : ");
+                buf = Console.ReadLine(); col = Int32.Parse(buf);
+            } while (col < 0 || Size <= col);
+            //phân phối vùng nhớ chứa array Banco và danh sach Nuocdi
+            Banco = new int[Size, Size];
+            Nuocdi = new ItemRec[Size * Size];
+            for (i = 0; i < Size * Size; i++)
+                Nuocdi[i] = new ItemRec();
+            //khởi ₫ộng trạng thái ₫ầu cho các vị trí 
+            for (i = 0; i < Size; i++)
+                for (j = 0; j < Size; j++)
+                    Banco[i, j] = -1; // con mã chưa ₫i qua ô [i,j]
+            SoNghiem = 0;
+            // Thiết lập nước ₫i ₫ầu tiên của con mã
+            Nuocdi[0].x = col;
+            Nuocdi[0].y = row;
+            Nuocdi[0].huong = 0;
+            SoNuocdi = 0;
+            //chọn khả năng ₫i tiếp của vị trí ₫ầu tiên của con mã
+            Banco[Nuocdi[SoNuocdi].x, Nuocdi[SoNuocdi].y] = 0;
+        }
+        //hàm tìm vị trí kế tiếp của con mã
+        // trả về TRUE nếu tìm ₫ược, FALSE nếu không tìm ₫ược 
+        static bool DiBuocNua()
+        {
+            int x = 0, y = 0;
+            bool RetVal;
+            RetVal = false;
+            do
+            { // lặp tìm vị trí kế
+                while (RetVal == false && Nuocdi[SoNuocdi].huong < 8)
+                {
+                    switch (Nuocdi[SoNuocdi].huong)
+                    { //thử hướng ₫i hiện tại
+                        case 0:
+                            x = Nuocdi[SoNuocdi].x + 2;
+                            y = Nuocdi[SoNuocdi].y - 1;
+                            break;
+                        case 1:
+                            x = Nuocdi[SoNuocdi].x + 1;
+                            y = Nuocdi[SoNuocdi].y - 2;
+                            break;
+                        case 2:
+                            x = Nuocdi[SoNuocdi].x - 1;
+                            y = Nuocdi[SoNuocdi].y - 2;
+                            break;
+                        case 3:
+                            x = Nuocdi[SoNuocdi].x - 2;
+                            y = Nuocdi[SoNuocdi].y - 1;
+                            break;
+                        case 4:
+                            x = Nuocdi[SoNuocdi].x - 2;
+                            y = Nuocdi[SoNuocdi].y + 1;
+                            break;
+                        case 5:
+                            x = Nuocdi[SoNuocdi].x - 1;
+                            y = Nuocdi[SoNuocdi].y + 2;
+                            break;
+                        case 6:
+                            x = Nuocdi[SoNuocdi].x + 1;
+                            y = Nuocdi[SoNuocdi].y + 2;
+                            break;
+                        case 7:
+                            x = Nuocdi[SoNuocdi].x + 2;
+                            y = Nuocdi[SoNuocdi].y + 1;
+                            break;
+                    }
+                    if (0 <= x && x < Size && 0 <= y && y < Size && Banco[x, y] == -1)
+                    {
+                        // nếu ₫ược thì ghi nhận
+                        SoNuocdi = SoNuocdi + 1;
+                        Banco[x, y] = SoNuocdi;
+                        Nuocdi[SoNuocdi].x = x;
+                        Nuocdi[SoNuocdi].y = y;
+                        Nuocdi[SoNuocdi].huong = 0;
+                        RetVal = true;
+                    }
+                    else // nếu không tìm ₫ược vị trí kế tiếp 
+                        Nuocdi[SoNuocdi].huong = Nuocdi[SoNuocdi].huong + 1;
+                }
+                if (RetVal == false && SoNuocdi != 0)
+                {
+                    // nếu không tìm ₫ược vị trí kế thì lùi con mã 1 bước
+                    Banco[Nuocdi[SoNuocdi].x, Nuocdi[SoNuocdi].y] = -1;
+                    SoNuocdi = SoNuocdi - 1;
+                    Nuocdi[SoNuocdi].huong = Nuocdi[SoNuocdi].huong + 1;
+                }
+            } while (!RetVal && (SoNuocdi != 0));
+            return RetVal;
+        }
+        // In kết quả con mã ₫i trên bàn cờ
+        static void InKetqua()
+        {
+            int h, c;
+            SoNghiem = SoNghiem + 1;
+            Console.WriteLine("Cách ₫i thứ : " + SoNghiem);
+            for (h = 0; h < Size; h++)
+            {
+                // Hiển thị hàng lưới ngang bàn cờ
+                for (c = 0; c < Size; c++) Console.Write("+----");
+                Console.WriteLine("+");
+                // Hiển thị nội dung hàng thứ h bàn cờ
+                for (c = 0; c < Size; c++)
+                    Console.Write("| {0:D2} ", Banco[h, c]);
+                Console.WriteLine("|");
+            }
+            // Hiển thị hàng lưới ngang cuối cùng của bàn cờ
+            for (c = 0; c < Size; c++) Console.Write("+----");
+            Console.WriteLine("+");
+        }
+    }//hết class Program
+}//hết namespace
